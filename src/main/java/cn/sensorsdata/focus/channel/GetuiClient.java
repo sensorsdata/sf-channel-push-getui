4614a1d6958c00b66c6bcd15c1813bee1a66869d
Dylan Liu <dylan@canva.com>
2021-04-27 17:16:17 +0800

    for list message too

--- src/main/java/cn/sensorsdata/focus/channel/GetuiClient.java
+++ src/main/java/cn/sensorsdata/focus/channel/GetuiClient.java
@@ -130,6 +130,9 @@ public class GetuiClient extends ChannelClient {
         // 离线有效时间，单位为毫秒，可选
         message.setOfflineExpireTime(MESSAGE_OFFLINE_EXPIRE_TIME);
 
+        // 设置默认厂商推送策略为不论现线或者离线都通过第三方厂商通道下发
+        message.setStrategyJson("{\"default\":2}");
+
         String failReason = null;
         try {
           // http://docs.getui.com/getui/server/java/push/
